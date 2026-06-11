d693bbcb337d5430a3727abbc8d5625b65e3dc86
lazare sharashenidze <sharashenidze2003@gmail.com>
2026-06-11 22:03:07 +0400

    Add lesson 5 classwork and homework

---
+++ homework/lesson 5/classwork/main.py
@@ -0,0 +1,17 @@
+try:
+    age = int(input("შეიყვანეთ ასაკი: "))
+
+    if age < 0:
+        raise TypeError("უარყოფითი ასაკი არ შეიძლება!")
+
+except TypeError as e:
+    print(e)
+
+except ValueError:
+    print("შეიყვანეთ მხოლოდ რიცხვი!")
+
+else:
+    if age < 18:
+        print("არასრულწლოვანი")
+    else:
+        print("სრულწლოვანი")
